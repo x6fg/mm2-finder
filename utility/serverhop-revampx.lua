1c6035166cdbc2f29d1e29cf1468bf2eb51156c8
x6fg <lucaspedgley0@gmail.com>
2025-02-14 23:13:20 +0300

    Update and rename serverhop-revamp.lua to serverhop-revampx.lua

--- utility/serverhop-revampx.lua
+++ utility/serverhop-revampx.lua
@@ -3,7 +3,7 @@ local AllIDs = {}
 local UsedIDs = {}
 local lastHour = os.date("!*t").hour
 local foundAnything = ""
-local serverFileName = "Sbaba" .. PlaceID .. ".json"  -- File name based on PlaceID
+local serverFileName = "server" .. PlaceID .. ".json"  -- File name based on PlaceID
 
 -- Load the previously stored IDs, timestamps, and lastHour
 local function loadData()
@@ -36,7 +36,7 @@ local function resetUsedIDsIfHourChanged()
         lastHour = currentHour
         delfile(serverFileName)
         task.wait(1)
-        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
     end
 end
 
