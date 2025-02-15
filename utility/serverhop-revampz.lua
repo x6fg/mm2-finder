56eccb83dc372a68f9d2b4f51605dff5001f09c5
x6fg <lucaspedgley0@gmail.com>
2025-02-15 09:15:16 +0300

    Update serverhop-revampz.lua

--- utility/serverhop-revampz.lua
+++ utility/serverhop-revampz.lua
@@ -34,8 +34,8 @@ local function resetUsedIDsIfHourChanged()
     if currentHour ~= lastHour then
         UsedIDs = {}
         lastHour = currentHour
-        saveData()  -- Save the new lastHour
-        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+        saveData()
+        task.wait(1)
     end
 end
 
